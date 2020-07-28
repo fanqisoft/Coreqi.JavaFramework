baf9539d01aa087da1dbc7566b57ac0343eee2c4
fanqi <fanqisoft@gmail.com>
2020-07-28 21:50:16 +0800

    修复SpringMVC配置未生效的问题

--- coreqi_java_framework_server/src/main/java/cn/coreqi/config/web/WebConfig.java
+++ coreqi_java_framework_server/src/main/java/cn/coreqi/config/web/WebConfig.java
@@ -1,8 +1,10 @@
 package cn.coreqi.config.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     //配置对静态资源的处理
     @Override
